bae68511ac5152712531dbf241c3d269b3fb38a0
Kisizawa <112299224+Kisizawa@users.noreply.github.com>
2022-10-04 19:58:07 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,10 @@
-pa
+public class Main {
+
+	public static void main(String[] args) {
+    double weight=64.5;
+    double height=1.7;
+    double bmi=weight/height/height;
+    System.out.println(String.format(%.1f),bmi);
+  
+  
+  }
